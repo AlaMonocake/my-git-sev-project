b0a26765cbe6e6d23452495d5888a27640d108d5
Alexandra <150329104+AlaMonocake@users.noreply.github.com>
2025-11-20 21:41:27 +0200

    Add files via upload

---
+++ my-sev-project/src/gr/aueb/sev/ch5/AddApp.java
@@ -0,0 +1,37 @@
+package gr.aueb.sev.ch5;
+
+/**
+ * Υπολογίζει το άθροισμα δύο ακεραίων
+ * με τη χρήση μεθόδου.
+ */
+public class AddApp {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = 0;
+
+        sum = add(a, b);
+
+        System.out.println("Sum: " + sum);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+
+//        int sum = 0;
+//
+//        sum = a + b;
+//
+//        return sum;
+    }
+
+    // Overload - υπερφόρτωση
+    public static long add(int a, long b) {
+        return a + b;
+    }
+
+    public static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
